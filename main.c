43a495b5bd9a58c071d31ffa06e1a55531fb98e3
Rillian Grant <rillian.grant@gmail.com>
2021-10-21 11:41:28 +0100

    Restructure

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include<stdlib.h>
 #include<stdio.h>
-#include<inttypes.h>
 
 unsigned short int *mem;
 // 16bit system gives 65536 (2^16) addressable memory locations.
@@ -11,9 +10,31 @@ static const int MEM_SIZE_WORDS = 65536;
 void load_binary_into_memory(char file[]) {
     printf("File: %s\n", file);
     FILE *init_file;
+    unsigned int addr;
     init_file = fopen(file, "rb");
-    fread(mem, MEM_SIZE_BYTES, 1, init_file);
+    if (init_file == NULL) {
+        printf("Error: The file %s could not be opened\n", file);
+        exit(1);
+    }
+
+    fread(&addr, 2, 1, init_file);
+    addr = __builtin_bswap16(addr);
+
+    fread(mem+addr, MEM_SIZE_BYTES, 1, init_file);
+
+    if (ferror(init_file)) {
+        printf("\n Error reading from file\n\n");
+        exit(1);
+    }
+
     fclose(init_file);
+}
+
+void dump_memory_to_binary() {
+    FILE *mem_dump_file;
+    mem_dump_file = fopen("dump.bin", "wb");
+    fwrite(mem, MEM_SIZE_BYTES, 1, mem_dump_file);
+    fclose(mem_dump_file);
 }
 
 int main(int argc, char *argv[]) {
@@ -27,19 +48,15 @@ int main(int argc, char *argv[]) {
         load_binary_into_memory(argv[i]);
     }
 
+    // TODO Check wether we need to do this
     for (unsigned int i=0; i<MEM_SIZE_WORDS; i++) {
         mem[i] = __builtin_bswap16(mem[i]);
     }
 
-    for (int i=0; i<MEM_SIZE_WORDS; i++) {
-        printf("%d: %d\n", i, mem[i]);
-    }
+    // Main code
 
     for (unsigned int i=0; i<MEM_SIZE_WORDS; i++) {
         mem[i] = __builtin_bswap16(mem[i]);
     }
-    FILE *mem_dump_file;
-    mem_dump_file = fopen("dump.bin", "wb");
-    fwrite(mem, MEM_SIZE_BYTES, 1, mem_dump_file);
-    fclose(mem_dump_file);
+    dump_memory_to_binary();
 }
